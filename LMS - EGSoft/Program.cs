7d7743b3ac810d8d33cfd4c58277bc48627f293d
edgomez18 <146097@p.uapa.edu.do>
2017-07-24 20:22:09 -0400

    Ninguna mejora importante.

--- LMS - EGSoft/Program.cs
+++ LMS - EGSoft/Program.cs
@@ -16,7 +16,7 @@ namespace LMS___EGSoft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new startseccion());
         }
     }
 }
